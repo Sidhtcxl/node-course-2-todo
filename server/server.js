1a83b68312e092f297a911f8c2d3ccb68fe93f61
Siddharth98 <siddharth.srivastava@w3villa.com>
2017-05-11 19:43:40 +0530

    Add Post /users route and token method

--- server/server.js
+++ server/server.js
@@ -26,17 +26,6 @@ app.post('/todos',(req,res)=>{
     });
 });
 
-app.post('/users',(req,res)=>{
-    var user = new User({
-        email:req.body.email
-    });
-    user.save().then((doc) =>{
-        res.send(doc);
-    },(e) => {
-        res.status(400).send(e);
-    });
-});
-
 app.get('/todos',(req,res) => {
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -93,6 +82,16 @@ app.patch('/todos/:id',(req,res) => {
   }).catch((e)=>{
     res.status(400).send();
   });
+});
+
+app.post('/users',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+  var user = new User(body);
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>res.status(400).send(e));
 });
 
 app.listen(port,()=>{
